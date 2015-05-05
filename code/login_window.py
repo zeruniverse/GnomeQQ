ede7e4520b73fd63e61aa84070ac80ed9bc041ca
gullanshire <3120000358@zju.edu.cn>
2015-05-05 12:50:42 +0000

    fixed some awkward mistake

--- code/login_window.py
+++ code/login_window.py
@@ -29,7 +29,7 @@ class LoginWindow(Gtk.Window):
         logging.info("Initializing login interface.")
 
         #Getting saved account info
-        self.get_saved_account_info(self)
+        self.get_saved_account_info()
 
         Gtk.Window.__init__(self, title="Login")
         self.set_border_width(20)
@@ -39,7 +39,7 @@ class LoginWindow(Gtk.Window):
 
         #Login button
         loginbutton_saved=Gtk.Button(label="Login")
-        loginbutton_saved.connect("clicked",self.on_login_saved_clicked,self)
+        loginbutton_saved.connect("clicked",self.on_login_saved_clicked)
         loginbutton_new=Gtk.Button(label="Login")
 
         #Stack for content
@@ -57,12 +57,12 @@ class LoginWindow(Gtk.Window):
             if counter==1:
                 buttontemp=Gtk.RadioButton.new_with_label_from_widget(None, i)
                 button.append(buttontemp)
-                button[counter-1].connect("toggled",self.saved_account_toggled,counter,self)
+                button[counter-1].connect("toggled",self.saved_account_toggled,counter)
                 saved_account_box.pack_start(button[counter-1],True,True,0)
             else:
                 buttontemp=Gtk.RadioButton.new_with_label_from_widget(button[0], i)
                 button.append(buttontemp)
-                button[counter-1].connect("toggled",self.saved_account_toggled,counter,self)
+                button[counter-1].connect("toggled",self.saved_account_toggled,counter)
                 saved_account_box.pack_start(button[counter-1],True,True,0)
             counter+=1
         #For login button
@@ -79,7 +79,7 @@ class LoginWindow(Gtk.Window):
         password.set_text("Password")
 
         ifsave=Gtk.CheckButton("Save")
-        ifsave.connect("clicked", self.ifsave_clicked,self)
+        ifsave.connect("clicked", self.ifsave_clicked)
         new_account_box_sub=Gtk.Box(spacing=6)
         new_account_box_sub.pack_start(ifsave,True,True,0)
         new_account_box_sub.pack_start(loginbutton_new,True,True,0)
@@ -96,73 +96,73 @@ class LoginWindow(Gtk.Window):
         vbox.pack_start(stack, True, True, 0)
 
         #New account login connect, for entry definition should be placed here
-        loginbutton_new.connect("clicked",self.on_login_new_clicked,self,qqnumber,password)
+        loginbutton_new.connect("clicked",self.on_login_new_clicked,qqnumber,password)
 
         #For test
-        #self.saved_account_test(self)
-        #self.write_account_info(self,"1","2")
+        #self.saved_account_test()
+        #self.write_account_info("1","2")
 
     #Login button click
     #Login button for saved account
-    def on_login_saved_clicked(self,button,father):
-        selection=father.__selection_number
+    def on_login_saved_clicked(self,button):
+        selection=self.__selection_number
         #For test
-        #print(selection,father.__account_qqnumber[selection-1],father.__account_encrypted_password[selection-1])
-        father.login(father.__account_qqnumber[selection-1],father.__account_encrypted_password[selection-1])
+        #print(selection,self.__account_qqnumber[selection-1],self.__account_encrypted_password[selection-1])
+        self.login(self.__account_qqnumber[selection-1],self.__account_encrypted_password[selection-1])
 
     #Login button for new account
-    def on_login_new_clicked(self,button,father,qqnumber,password):
+    def on_login_new_clicked(self,button,qqnumber,password):
         qid=qqnumber.get_text()
         pw=password.get_text()
-        if father.__ifsave==1:
-            father.write_account_info(father,qid,pw)
-        father.login(qid,pw)
+        if self.__ifsave==1:
+            self.write_account_info(qid,pw)
+        self.login(qid,pw)
 
     #Save or not button
-    def ifsave_clicked(self,button,father):
+    def ifsave_clicked(self,button):
         if button.get_active():
-            father.__ifsave=1
+            self.__ifsave=1
         else:
-            father.__ifsave=0
+            self.__ifsave=0
 
     #Dealing with ratiobutton conflict
     #need modification
-    def saved_account_toggled(self,button,name,father):
+    def saved_account_toggled(self,button,name):
         if button.get_active():
-            father.__selection_number=name
+            self.__selection_number=name
 
     #Getting saved account information form file
-    def get_saved_account_info(self,father):
+    def get_saved_account_info(self):
         logging.info("Getting saved account info.")
         f=open('accountinfo','r')
         count=1
         for line in f:
             if count % 2:
                 #rstrip is to remove \n from the file
-                father.__account_qqnumber.append(line.rstrip('\n'))
+                self.__account_qqnumber.append(line.rstrip('\n'))
                 count+=1
             else:
-                father.__account_encrypted_password.append(line.rstrip('\n'))
+                self.__account_encrypted_password.append(line.rstrip('\n'))
                 count+=1
-        father.__account_number=len(father.__account_qqnumber)
+        self.__account_number=len(self.__account_qqnumber)
         f.close()
 
     #Testing the saved account reading module
-    def saved_account_test(self,father):
+    def saved_account_test(self):
         print("account saved qqnumber")
-        for a in father.__account_qqnumber:
+        for a in self.__account_qqnumber:
             print a
         print("account saved password")
-        for a in father.__account_encrypted_password:
+        for a in self.__account_encrypted_password:
             print a
 
     #Write new account information to file
-    def write_account_info(self,father,number,password):
+    def write_account_info(self,number,password):
         logging.info("Saving new account information.")
         f=open('accountinfo','w')
-        temp_account_number=len(father.__account_qqnumber)
+        temp_account_number=len(self.__account_qqnumber)
         for i in range(0,temp_account_number):
-            f.write(father.__account_qqnumber[i]+"\n"+father.__account_encrypted_password[i]+"\n")
+            f.write(self.__account_qqnumber[i]+"\n"+self.__account_encrypted_password[i]+"\n")
         f.write(number+"\n")
         f.write(password+"\n")
         f.close()
